f0fb9db079a388b293b73620b76bb4eae80f83e8
jonnadaindhu <119721592+jonnadaindhu@users.noreply.github.com>
2023-06-16 19:50:55 +0530

    import java.util.*; public class praveen{ public static void main(String args[]){     int a;     Scanner s=new Scanner(System.in);     a=s.nextInt();     for(int i=1;i<=a;i++)     {         for(int j=a;j>=1;j--)         System.out.print(j+" ");         System.out.println();     } } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,20 +1,15 @@
 import java.util.*;
- public class praveen{
+public class praveen{
 public static void main(String args[]){
     int a;
     Scanner s=new Scanner(System.in);
     a=s.nextInt();
     for(int i=1;i<=a;i++)
     {
-        for(int j=1;j<=a;j++)
-        {
-            if(i==j)
-            System.out.print("0");
-            else
-            System.out.print("x");
-            
-        }
+        for(int j=a;j>=1;j--)
+        System.out.print(j+" ");
         System.out.println();
     }
 }
 }
+
