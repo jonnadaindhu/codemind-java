7d83573d43902d5725921d75269515c7fa18a784
jonnadaindhu <119721592+jonnadaindhu@users.noreply.github.com>
2023-06-16 18:37:49 +0530

    import java.util.*;  public class praveen{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         char a;         a=sc.next().charAt(0);         int n=a;         System.out.print(n);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class praveen{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char a;
+        a=sc.next().charAt(0);
+        int n=a;
+        System.out.print(n);
+    }
+}
+
