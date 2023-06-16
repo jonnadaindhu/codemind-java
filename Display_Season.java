78a37f58a0e42b1c1074b5a7f5a1cb7ecf45ce36
jonnadaindhu <119721592+jonnadaindhu@users.noreply.github.com>
2023-06-16 19:00:51 +0530

    import java.util.*;  public class praveen{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,a;         n=sc.nextInt();         if(n==4 || n==5 ||n==6)         System.out.println("Summer");         else if(n==7 || n==8 ||n==9)         System.out.println("Rainy");         else if(n==11 || n==12 ||n==1)         System.out.println("Winter");         else if(n==2 || n==3)         System.out.println("Spring");         else         System.out.println(-1);     } }

---
+++ Display_Season.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ public class praveen{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,a;
+        n=sc.nextInt();
+        if(n==4 || n==5 ||n==6)
+        System.out.println("Summer");
+        else if(n==7 || n==8 ||n==9)
+        System.out.println("Rainy");
+        else if(n==11 || n==12 ||n==1)
+        System.out.println("Winter");
+        else if(n==2 || n==3)
+        System.out.println("Spring");
+        else
+        System.out.println(-1);
+    }
+}
